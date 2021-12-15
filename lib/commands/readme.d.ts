e03dd1d0f7e4b77c6e4bc8d5c25e5ccf8b1a9625
Pierluigi Viti <pierluigi@commercelayer.io>
2021-12-15 12:52:50 +0100

    Fix command bin

--- lib/commands/readme.d.ts
+++ lib/commands/readme.d.ts
@@ -6,6 +6,7 @@ export default class Readme extends Command {
         dir: flags.IOptionFlag<string>;
         multi: import("@oclif/parser/lib/flags").IBooleanFlag<boolean>;
         plugin: import("@oclif/parser/lib/flags").IBooleanFlag<boolean>;
+        bin: flags.IOptionFlag<string | undefined>;
     };
     run(): Promise<void>;
     replaceTag(readme: string, tag: string, body: string): string;
@@ -13,7 +14,7 @@ export default class Readme extends Command {
     usage(config: Config.IConfig): string;
     multiCommands(config: Config.IConfig, commands: Config.Command[], dir: string): string;
     createTopicFile(file: string, config: Config.IConfig, topic: Config.Topic, commands: Config.Command[]): void;
-    commands(config: Config.IConfig, commands: Config.Command[], plugin?: boolean): string;
+    commands(config: Config.IConfig, commands: Config.Command[]): string;
     renderCommand(config: Config.IConfig, c: Config.Command): string;
     commandCode(config: Config.IConfig, c: Config.Command): string | undefined;
     private repo;
